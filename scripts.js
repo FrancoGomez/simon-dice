6fee3bd9d7bcfd93a8851704a717a27e290e7001
Franco <mgomez21699@gmail.com>
2021-10-23 18:52:19 -0300

    El juego comienza definiendo el numero de ronda y de puntos.
    Luego se crea un array que ira recolectando numeros generados al azar, y una variable que ira aumentando con el numero de clicks correctos que de el usuario.
    
    Por lo que el primer numero en el array es generado y una funcion reproduce lo que hay en ese array, dentro de la pantalla.
    
    Mientras esto sucede los clicks que da el usuario no hacen nada por una funcion que los bloquea.
    
    Luego de que la secuencia termina de ser, al usuario se le desbloquea la capacidad de que sus clicks hagan efecto, y por cada click una funcion va comprobando que lo clickeado sean los cuadrados presentados, en el orden presentado.
    
    En caso de ser esto asi, un nuevo numero random es agregado al array, y una funcion vuelve a representalo en la pantalla... etc. Y esto sucedera hasta que el usuario cometa un error, ya sea en el cuadrado que clickeo o el orden en que lo hizo.
    
    Cuando esto suceda el juego volvera a comenzar, dandosele a todas las variables su valor inicial.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
-
-//NOMENCLATURA VARIABLES
+// NOMENCLATURA VARIABLES
 //-----------------------------------------------------------------------------
 // En caso de que la variable contenga un elemento del html, comenzara con '$'.
 //-----------------------------------------------------------------------------
@@ -13,8 +12,12 @@
 const $contenedorCuadradosJuego = document.querySelector(
   "#contenedor-cuadrados-juego"
 );
+const $numeroRonda = document.querySelector("#numero-ronda");
+const $numeroPuntos = document.querySelector("#numero-puntos");
 
-const NUMERO_CUADRADOS = 9
+const NUMERO_CUADRADOS = 9;
+let arrayPatronNumerosSimonDice;
+let numeroDeClick;
 
 function crearCuadradosJuego(cuadradosParaCrear) {
   for (let index = 0; index < cuadradosParaCrear; index++) {
@@ -27,10 +30,85 @@ function crearCuadradosJuego(cuadradosParaCrear) {
 
 crearCuadradosJuego(NUMERO_CUADRADOS);
 
-$contenedorCuadradosJuego.onclick = function (evento) {
-  if (evento.target.className === "cuadrados-juego cuadrados-juego--default") {
+function agregarNumeroArrayPatronNumerosSimonDice() {
+  const numeroRandom = Math.floor(Math.random() * NUMERO_CUADRADOS);
+  arrayPatronNumerosSimonDice.push(numeroRandom);
+}
+
+function resaltarCuadrado($cuadrado) {
+  $cuadrado.className = "cuadrados-juego cuadrados-juego--activo";
+
+  setTimeout(function () {
+    $cuadrado.className = "cuadrados-juego cuadrados-juego--default";
+  }, 500);
+}
+
+function bloquearClickUsuario() {
+  $contenedorCuadradosJuego.onclick = () => {};
+}
+
+function desbloquearClickUsuario() {
+  $contenedorCuadradosJuego.onclick = (evento) => {
+    manejarClickUsuario(evento);
+  };
+}
+
+function reproducirPatronSimonDicenEnPantalla() {
+  const RETRASO_TURNO_JUGADOR = 1000 * (arrayPatronNumerosSimonDice.length + 1);
+  bloquearClickUsuario();
+
+  arrayPatronNumerosSimonDice.forEach(function (posicionEnArray, index) {
+    const SEGUNDOS_CUADRADO_ILUMINADO = 1000 * (index + 1);
+    let $cuadrado = $contenedorCuadradosJuego.childNodes[posicionEnArray];
+
+    setTimeout(function () {
+      resaltarCuadrado($cuadrado);
+    }, SEGUNDOS_CUADRADO_ILUMINADO);
+  });
+
+  setTimeout(function () {
+    desbloquearClickUsuario();
+  }, RETRASO_TURNO_JUGADOR);
+}
+
+function iniciarJuego() {
+  $numeroRonda.textContent = 1;
+  $numeroPuntos.textContent = 0;
+  arrayPatronNumerosSimonDice = [];
+  numeroDeClick = 0;
+  agregarNumeroArrayPatronNumerosSimonDice();
+  reproducirPatronSimonDicenEnPantalla();
+}
+
+iniciarJuego();
+
+function revisarSiClickeoCuadradoCorrecto(
+  indexParaRevisarEnArray,
+  idCuadradoClickeado
+) {
+  return (
+    arrayPatronNumerosSimonDice[indexParaRevisarEnArray] ===
+    Number(idCuadradoClickeado)
+  );
+}
+
+function manejarClickUsuario(evento) {
+  if (revisarSiClickeoCuadradoCorrecto(numeroDeClick, evento.target.id)) {
     evento.target.className = "cuadrados-juego cuadrados-juego--activo";
+    numeroDeClick++;
+    $numeroPuntos.textContent = Number($numeroPuntos.textContent) + 1;
+
+    setTimeout(function () {
+      evento.target.className = "cuadrados-juego cuadrados-juego--default";
+    }, 500);
+
+    if (arrayPatronNumerosSimonDice.length === numeroDeClick) {
+      $numeroRonda.textContent = Number($numeroRonda.textContent) + 1;
+      numeroDeClick = 0;
+      agregarNumeroArrayPatronNumerosSimonDice();
+      reproducirPatronSimonDicenEnPantalla();
+    }
   } else {
-    evento.target.className = "cuadrados-juego cuadrados-juego--default";
+    iniciarJuego();
   }
-};
+}
